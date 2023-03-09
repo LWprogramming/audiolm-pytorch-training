49658c4d6aaa1f4b2c021122ab763caaddcf9755
Leon Wu <lwgithub@gmail.com>
2023-03-08 17:54:08 -0800

    oops

--- hubert_ckpt_download.py
+++ hubert_ckpt_download.py
@@ -2,14 +2,13 @@ import os
 import urllib.request
 
 prefix = "/fsx/itsleonwu/audiolm-pytorch"
-dataset_folder = f"{prefix}/placeholder_dataset"
 hubert_ckpt = f'hubert/hubert_base_ls960.pt'
 hubert_quantizer = f'hubert/hubert_base_ls960_L9_km500.bin' # listed in row "HuBERT Base (~95M params)", column Quantizer
 
 # hubert checkpoints can be downloaded at
 # https://github.com/facebookresearch/fairseq/tree/main/examples/hubert
 if not os.path.isdir(f"{prefix}/hubert"):
-  os.makedirs("{prefix}/hubert")
+  os.makedirs(f"{prefix}/hubert")
 if not os.path.isfile(f"{prefix}/{hubert_ckpt}"):
   hubert_ckpt_download = f"https://dl.fbaipublicfiles.com/{hubert_ckpt}"
   urllib.request.urlretrieve(hubert_ckpt_download, f"{prefix}/{hubert_ckpt}")
