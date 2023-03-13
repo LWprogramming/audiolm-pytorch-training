96162f414cb780272084a824def8d52637686ca2
Leon Wu <lwgithub@gmail.com>
2023-03-13 16:06:23 -0700

    Try using patched audiolm

---
+++ use_patched_audiolm.py
@@ -0,0 +1,28 @@
+# use the personal_hacks branch of my fork of the repo to get print statements, etc etc.
+
+import urllib.request
+import os
+import zipfile
+import subprocess
+
+zip_file_path = "audiolm-pytorch-personal-hacks.zip"
+if os.path.isfile(zip_file_path):
+    replace = input("personal hacks zip already exists. replacing (as well as audiolm_pytorch library...)")
+    os.remove(zip_file_path)
+    shutil.rmtree("audiolm_pytorch")
+urllib.request.urlretrieve("https://github.com/LWProgramming/audiolm-pytorch/archive/refs/heads/personal_hacks.zip", zip_file_path)
+
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall("audiolm-pytorch")
+
+# move library itself to current directory
+subprocess.run(['mv', 'audiolm-pytorch/audiolm-pytorch-personal_hacks/audiolm_pytorch', '.'])
+
+# move setup.py to current directory for requirements
+subprocess.run(['mv', 'audiolm-pytorch/audiolm-pytorch-personal_hacks/setup.py', '.'])
+
+# install requirements from the patched audiolm-pytorch directory
+subprocess.run(['pip', 'install', '.'])
+
+# remove the rest of the audiolm-pytorch directory
+subprocess.run(['rm', '-rf', 'audiolm-pytorch'])
