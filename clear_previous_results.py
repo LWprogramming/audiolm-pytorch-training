a2b46fb1c5eae7b88aaa53dfe3bcb29ba309fa5d
Leon Wu <lwgithub@gmail.com>
2023-03-13 16:23:29 -0700

    Create clear previous results script

---
+++ clear_previous_results.py
@@ -0,0 +1,13 @@
+# assuming running from audiolm-pytorch-training and results in audiolm-pytorch-results
+
+import os
+import shutil
+
+results_folder = "../audiolm-pytorch-results"
+if not os.path.isdir(results_folder):
+	raise AssertionError("didn't find results_folder, no results to clear out")
+
+shutil.rmtree(f"{results_folder}/coarse_results")
+shutil.rmtree(f"{results_folder}/fine_results")
+shutil.rmtree(f"{results_folder}/semantic_results")
+shutil.rmtree(f"{results_folder}/soundstream_results")
