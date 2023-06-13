c31062877805a7cf4193f048a114b035f0d2773a
Leon Wu <lwgithub@gmail.com>
2023-06-12 17:06:22 -0700

    s3 backup script

---
+++ aws_ckpt_backup_script.py
@@ -0,0 +1,28 @@
+import sys
+import re
+import os
+import subprocess
+
+job_id = sys.argv[1]
+
+# Read output log
+with open(f"/fsx/itsleonwu/audiolm-pytorch-results/output-{job_id}.log", "r") as f:
+    output_log = f.read()
+
+# Extract audiolm version
+version_match = re.search(r"training on audiolm_pytorch version (\d+\.\d+\.\d+)", output_log)
+audiolm_version = version_match.group(1)
+
+# Define S3 bucket folder
+s3_folder = f"slurm-job-{job_id}-audiolm-{audiolm_version}"
+
+# Transfer output and error logs
+# I made the bucket back when I was trying paperspace and now it's my catch-all for any temporary ML stuff that I need to back up, oops... naming things is hard
+subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/output-{job_id}.log", f"s3://itsleonwu-paperspace/{s3_folder}/output-{job_id}.log", "--profile", "laion-stability-my-s3-bucket"])
+subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/error-{job_id}.log", f"s3://itsleonwu-paperspace/{s3_folder}/error-{job_id}.log", "--profile", "laion-stability-my-s3-bucket"])
+
+# Transfer checkpoints
+for folder in ["semantic_results", "coarse_results", "fine_results"]:
+    folder_path = f"/fsx/itsleonwu/audiolm-pytorch-results/{folder}_{job_id}"
+    for file in os.listdir(folder_path):
+        subprocess.run(["aws", "s3", "cp", f"{folder_path}/{file}", f"s3://itsleonwu-paperspace/{s3_folder}/{folder}/{file}", "--profile", "laion-stability-my-s3-bucket"])
