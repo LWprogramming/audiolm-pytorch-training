0415436eb6c095613c55ad702561a5885a68e842
Leon Wu <lwgithub@gmail.com>
2023-06-14 14:42:16 -0700

    debug specific job continued

--- sbatch_job.sh
+++ sbatch_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=1
-#SBATCH --job-name=audiolm-e2e
+#SBATCH --job-name=audiolm-e2e-fine-generate-debug
 #SBATCH --comment=laion
 #SBATCH --open-mode=append
 #SBATCH --output=../audiolm-pytorch-results/output-%A.log
@@ -16,4 +16,7 @@ echo "SLURM_JOB_ID: $SLURM_JOB_ID" >> ../audiolm-pytorch-results/output-$SLURM_J
 
 # export CUBLAS_WORKSPACE_CONFIG=:4096:8 # increase memory footprint by about 24 MiB but gives deterministic results. See https://docs.nvidia.com/cuda/cublas/index.html#results-reproducibility
 
-python -u audiolm_pytorch_demo_laion.py "$@" --slurm_job_id $SLURM_JOB_ID
+# export CUDA_LAUNCH_BLOCKING=1
+source venv/bin/activate # in case this hasn't already been done
+# python -u audiolm_pytorch_demo_laion.py "$@" --slurm_job_id $SLURM_JOB_ID
+python -u audiolm_pytorch_demo_laion.py "$@" --slurm_job_id 280087
