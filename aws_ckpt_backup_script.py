52f142d47e727c77fdd892e4fc56cf8d13dbf4c5
Leon Wu <lwgithub@gmail.com>
2023-06-16 19:29:15 -0700

    add generation

--- aws_ckpt_backup_script.py
+++ aws_ckpt_backup_script.py
@@ -20,12 +20,14 @@ audiolm_version = version_match.group(1)
 # Define S3 bucket folder
 s3_folder = f"slurm-job-{job_id}-audiolm-{audiolm_version}"
 
-# Transfer output and error logs, along with the scripts to run the code.
+# Transfer output and error logs, generation, along with the scripts to run the code.
 # I made the bucket back when I was trying paperspace and now it's my catch-all for any temporary ML stuff that I need to back up, oops... naming things is hard
 subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-training/sbatch_job.sh", f"s3://itsleonwu-paperspace/{s3_folder}/sbatch_job.sh", "--profile", "laion-stability-my-s3-bucket"])
 subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-training/audiolm_pytorch_demo_laion.py", f"s3://itsleonwu-paperspace/{s3_folder}/audiolm_pytorch_demo_laion.py", "--profile", "laion-stability-my-s3-bucket"])
 subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/output-{job_id}.log", f"s3://itsleonwu-paperspace/{s3_folder}/output-{job_id}.log", "--profile", "laion-stability-my-s3-bucket"])
 subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/error-{job_id}.log", f"s3://itsleonwu-paperspace/{s3_folder}/error-{job_id}.log", "--profile", "laion-stability-my-s3-bucket"])
+subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/out_{job_id}.wav", f"s3://itsleonwu-paperspace/{s3_folder}/error-{job_id}.log", "--profile", "laion-stability-my-s3-bucket"])
+
 
 # Transfer checkpoints
 for folder in ["semantic_results", "coarse_results", "fine_results"]:
