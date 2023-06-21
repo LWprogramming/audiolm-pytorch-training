0d8a347f8e0fecb0e8fb82388dc440c5478d484b
Leon Wu <lwgithub@gmail.com>
2023-06-20 19:24:23 -0700

    fix for openslr run

--- audiolm_pytorch_demo_laion.py
+++ audiolm_pytorch_demo_laion.py
@@ -51,7 +51,6 @@ dataset_folder = "/fsx/itsleonwu/audiolm-pytorch-datasets/openslr-slr12-dev-clea
 # dataset_folder = "/fsx/itsleonwu/audiolm-pytorch-datasets/cocochorales_samples"
 # dataset_folder = "/fsx/itsleonwu/audiolm-pytorch-datasets/two_identical_copies_of_cocochorales_single_sample"
 # resample the given sample to 24kHz to work with encodec and then trim it so we take only the first second of audio, so the transformer actually only sees the same data every single time
-dataset_folder = "/fsx/itsleonwu/audiolm-pytorch-datasets/two_identical_copies_of_cocochorales_single_sample_resampled_24kHz_trimmed_first_second"
 # dataset_folder = "/fsx/itsleonwu/audiolm-pytorch-datasets/two_identical_copies_of_cocochorales_single_sample_resampled_24kHz_trimmed_first_second"
 hubert_ckpt = f'hubert/hubert_base_ls960.pt'
 hubert_quantizer = f'hubert/hubert_base_ls960_L9_km500.bin' # listed in row "HuBERT Base (~95M params)", column Quantizer
@@ -166,8 +165,8 @@ wav2vec = HubertWithKmeans(
     kmeans_path = f"{prefix}/{hubert_quantizer}"
 )
 
-num_train_steps = 5001
-save_every = 1000
+num_train_steps = 1000001
+save_every = 100000
 
 semantic_transformer = SemanticTransformer(
     num_semantic_tokens = wav2vec.codebook_size,
