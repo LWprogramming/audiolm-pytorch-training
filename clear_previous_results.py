173ff441c762cbb7f47c1f4b38be2ae2346f7c37
Leon Wu <lwgithub@gmail.com>
2023-06-29 22:34:33 -0700

    update clear script to clear only specific jobs

--- clear_previous_results.py
+++ clear_previous_results.py
@@ -3,7 +3,14 @@
 import os
 import shutil
 import re
+import argparse
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Clear out specific training job results.')
+parser.add_argument('job_ids', type=int, nargs='+', help='List of training job ids to clear.')
+args = parser.parse_args()
+
+# Confirm deletion
 x = input("Are you SURE you want to wipe out previous results? Type \"absolutely yes\" if so")
 if not x == "absolutely yes":
     raise AssertionError("nope")
@@ -16,17 +23,19 @@ prefixes = [
     "coarse_results",
     "fine_results",
     "semantic_results",
-    "soundstream_results",
-    "placeholder_dataset",
 ]
 
-for item in os.listdir(results_folder):
-    item_path = os.path.join(results_folder, item)
-    for prefix in prefixes:
-        if item.startswith(prefix):
-            if os.path.isdir(item_path):
-                shutil.rmtree(item_path)
-            break
-    if re.match(r'(output|error)-\d+\.log', item):
-        if os.path.isfile(item_path):
-            os.remove(item_path)
+for job_id in args.job_ids:
+    for item in os.listdir(results_folder):
+        item_path = os.path.join(results_folder, item)
+        for prefix in prefixes:
+            if item.startswith(f"{prefix}_{job_id}"):
+                if os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
+                break
+        if re.match(rf'(output|error)-{job_id}\.log', item):
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+
+# Usage: python script.py [job_id1] [job_id2] ... [job_idN]
+# Example: python script.py 123 456 789
