fba683656a9ddc5ca92e93ccbc1192990d91631b
Leon Wu <lwgithub@gmail.com>
2023-07-03 18:00:00 -0700

    oops

--- sbatch_job.sh
+++ sbatch_job.sh
@@ -17,8 +17,8 @@
 # Sometimes slurm jobs get pre-empted. If this ends up happening, we want to have two things recorded: the current training script, so we can properly restart training (in case there were breaking changes previously made). It'd take a good bit more effort to save a separate audiolm_pytorch version for each run, while the API for that doesn't change much, so I'm going to skip that for now.
 # See also https://twitter.com/miraculous_cake/status/1676003814372151297
 # This is somewhat like the fork() function in unix operating systems
-if [[ -f audiolm_pytorch_demo_laion_$SLURM_JOB_ID.py && -f sbatch_script_$SLURM_JOB_ID.sh ]]; then
-    echo "SLURM_JOB_ID: $SLURM_JOB_ID" >> ../audiolm-pytorch-results/output-$SLURM_JOB_ID.log
+if [[ -f audiolm_pytorch_demo_laion_$SLURM_JOB_ID.py && -f sbatch_job_$SLURM_JOB_ID.sh ]]; then
+  echo "SLURM_JOB_ID: $SLURM_JOB_ID" >> ../audiolm-pytorch-results/output-$SLURM_JOB_ID.log
 	source venv/bin/activate # in case this hasn't already been done
 	# export CUBLAS_WORKSPACE_CONFIG=:4096:8 # increase memory footprint by about 24 MiB but gives deterministic results. See https://docs.nvidia.com/cuda/cublas/index.html#results-reproducibility
 	# export CUDA_LAUNCH_BLOCKING=1
@@ -53,6 +53,6 @@ if [[ -f audiolm_pytorch_demo_laion_$SLURM_JOB_ID.py && -f sbatch_script_$SLURM_
 	python -u audiolm_pytorch_demo_laion_$OVERRIDABLE_SLURM_JOB_ID.py --run_mode $RUN_MODE $WITH_PROFILING --slurm_job_id $OVERRIDABLE_SLURM_JOB_ID --parallel_training
 else
   cp audiolm_pytorch_demo_laion.py audiolm_pytorch_demo_laion_$SLURM_JOB_ID.py
-  cp sbatch_script.sh sbatch_script_$SLURM_JOB_ID.sh
-	sbatch sbatch_script_$SLURM_JOB_ID.sh "$@"
+  cp sbatch_job.sh sbatch_job_$SLURM_JOB_ID.sh
+	sbatch sbatch_job_$SLURM_JOB_ID.sh "$@"
 fi
