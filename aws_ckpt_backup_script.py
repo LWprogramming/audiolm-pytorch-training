646c249ac0013966ef5eb0bdf79ccbaf203d8e00
Leon Wu <lwgithub@gmail.com>
2023-07-03 17:16:05 -0700

    update aws backup script too

--- aws_ckpt_backup_script.py
+++ aws_ckpt_backup_script.py
@@ -30,8 +30,8 @@ s3_folder = f"slurm-job-{job_id}-audiolm-{audiolm_version}"
 
 # Transfer output and error logs, generation, along with the scripts to run the code.
 # I made the bucket back when I was trying paperspace and now it's my catch-all for any temporary ML stuff that I need to back up, oops... naming things is hard
-subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-training/sbatch_job.sh", f"s3://{bucket_prefix}/{s3_folder}/sbatch_job.sh", "--profile", "laion-stability-my-s3-bucket"])
-subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-training/audiolm_pytorch_demo_laion.py", f"s3://{bucket_prefix}/{s3_folder}/audiolm_pytorch_demo_laion.py", "--profile", "laion-stability-my-s3-bucket"])
+subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-training/sbatch_job_{job_id}.sh", f"s3://{bucket_prefix}/{s3_folder}/sbatch_job.sh", "--profile", "laion-stability-my-s3-bucket"])
+subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-training/audiolm_pytorch_demo_laion_{job_id}.py", f"s3://{bucket_prefix}/{s3_folder}/audiolm_pytorch_demo_laion.py", "--profile", "laion-stability-my-s3-bucket"])
 subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/output-{job_id}.log", f"s3://{bucket_prefix}/{s3_folder}/output-{job_id}.log", "--profile", "laion-stability-my-s3-bucket"])
 subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/error-{job_id}.log", f"s3://{bucket_prefix}/{s3_folder}/error-{job_id}.log", "--profile", "laion-stability-my-s3-bucket"])
 output_filename = f"out_job_id_{job_id}_step_{step}.wav" # should match what is saved in audiolm_pytorch_demo_laion.py
--- sbatch_job.sh
+++ sbatch_job.sh
@@ -17,7 +17,7 @@
 # Sometimes slurm jobs get pre-empted. If this ends up happening, we want to have two things recorded: the current training script, so we can properly restart training (in case there were breaking changes previously made). It'd take a good bit more effort to save a separate audiolm_pytorch version for each run, while the API for that doesn't change much, so I'm going to skip that for now.
 # See also https://twitter.com/miraculous_cake/status/1676003814372151297
 # This is somewhat like the fork() function in unix operating systems
-if [[ -f audiolm_pytorch_demo_laion_snapshot_$SLURM_JOB_ID.py && -f sbatch_script_$SLURM_JOB_ID.sh ]]; then
+if [[ -f audiolm_pytorch_demo_laion_$SLURM_JOB_ID.py && -f sbatch_script_$SLURM_JOB_ID.sh ]]; then
     echo "SLURM_JOB_ID: $SLURM_JOB_ID" >> ../audiolm-pytorch-results/output-$SLURM_JOB_ID.log
 	source venv/bin/activate # in case this hasn't already been done
 	# export CUBLAS_WORKSPACE_CONFIG=:4096:8 # increase memory footprint by about 24 MiB but gives deterministic results. See https://docs.nvidia.com/cuda/cublas/index.html#results-reproducibility
@@ -27,7 +27,7 @@ if [[ -f audiolm_pytorch_demo_laion_snapshot_$SLURM_JOB_ID.py && -f sbatch_scrip
 	echo "run mode: " $RUN_MODE
 	python -u audiolm_pytorch_demo_laion.py --slurm_job_id $SLURM_JOB_ID --run_mode $RUN_MODE --parallel_training $WITH_PROFILING
 else
-    cp audiolm_pytorch_demo_laion_snapshot.py audiolm_pytorch_demo_laion_snapshot_$SLURM_JOB_ID.py
+    cp audiolm_pytorch_demo_laion.py audiolm_pytorch_demo_laion_$SLURM_JOB_ID.py
     cp sbatch_script.sh sbatch_script_$SLURM_JOB_ID.sh
 	sbatch sbatch_script_$SLURM_JOB_ID.sh "$@"
 fi
