19b4fe2338a2ae12ef6763199327c92a62616f58
Leon Wu <lwgithub@gmail.com>
2023-07-13 16:28:02 -0700

    Revert "typing"
    This reverts commit 2e1cebcab4c339776eff26dce75f6e337f72c309.

--- audiolm_pytorch_demo_laion.py
+++ audiolm_pytorch_demo_laion.py
@@ -232,7 +232,7 @@ codec = EncodecWrapper()
 
 #############
 
-accelerate_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True).to_kwargs()
+accelerate_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
 # this argument for avoiding error that indicates that your module has parameters that were not used in producing loss
 # see link: https://github.com/huggingface/accelerate/issues/24#issuecomment-814106927
 
