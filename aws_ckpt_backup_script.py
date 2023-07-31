d4b3c765e84dab104a8f0fd6fbe489ce6bc325d8
Leon Wu <lwgithub@gmail.com>
2023-07-31 13:08:54 -0700

    aws backup script separate checkpiont and job ids

--- aws_ckpt_backup_script.py
+++ aws_ckpt_backup_script.py
@@ -3,18 +3,20 @@ import re
 import os
 import subprocess
 
-# usage: python aws_ckpt_backup_script.py <job_id> <bucket_prefix optional>
+# usage: python aws_ckpt_backup_script.py <job_id> <checkpoint_job_id> <bucket_prefix optional>
 
 # aws cp commands in shell look like aws s3 cp /fsx/itsleonwu/audiolm-pytorch-training/sbatch_job_1.sh s3://s-laion/itsleonwu/slurm-job-1-audiolm-0.1.0/sbatch_job.sh --profile laion-stability-my-s3-bucket
 
 # backs up checkpoints to s3
 # my personal bucket is named "itsleonwu-laion"
 
+# job id is the job that generated the script, checkpoint_job_id is the id of the folders with checkpoints. If these are the same, they should appear twice. They differ when you start a new job that continues from an old folder's checkpoint
 job_id = sys.argv[1]
-if len(sys.argv) == 3:
-    bucket_prefix = sys.argv[2]
+checkpoint_job_id = sys.argv[2]
+if len(sys.argv) >= 4:
+    bucket_prefix = sys.argv[3]
 else:
-    bucket_prefix = "s-laion/itsleonwu" # default into my own personal folder 
+    bucket_prefix = "s-laion/itsleonwu"  # default into my own personal folder
 
 description = input("Write a description or leave it blank if you don't want to:")
 with open(f"/fsx/itsleonwu/audiolm-pytorch-results/{job_id}-description.txt", "w") as f:
@@ -29,7 +31,7 @@ version_match = re.search(r"training on audiolm_pytorch version (\d+\.\d+\.\d+)"
 audiolm_version = version_match.group(1)
 
 # Define S3 bucket folder
-s3_folder = f"slurm-job-{job_id}-audiolm-{audiolm_version}"
+s3_folder = f"slurm-job-{checkpoint_job_id}-audiolm-{audiolm_version}"
 
 # Transfer output and error logs, generation, along with the scripts to run the code.
 # I made the bucket back when I was trying paperspace and now it's my catch-all for any temporary ML stuff that I need to back up, oops... naming things is hard
@@ -43,7 +45,7 @@ subprocess.run(["aws", "s3", "cp", f"/fsx/itsleonwu/audiolm-pytorch-results/erro
 
 # Transfer checkpoints
 for folder in ["semantic_results", "coarse_results", "fine_results"]:
-    full_folder_name = f"{folder}_{job_id}"
+    full_folder_name = f"{folder}_{checkpoint_job_id}"
     folder_path = f"/fsx/itsleonwu/audiolm-pytorch-results/{full_folder_name}"
     if os.path.exists(folder_path):
         # probably only one of these will exist if we're training different transformers in different jobs
